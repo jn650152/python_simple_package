28a8a7f3872f0db1a1f3b152e78009c3aa6b5f18
jluo <jn650152@gmail.com>
2019-07-25 08:26:59 -0400

    Created the minimum structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'apackage',
+      version = '0.1',
+      description = 'apackage description',
+      url = 'https://github.com/jn650152/python_simple_package',
+      author = 'jluo',
+      author_email = 'jn650152@dal.ca',
+      license = 'MIT',
+      packages = ['apackage'],
+      zip_safe = False)
